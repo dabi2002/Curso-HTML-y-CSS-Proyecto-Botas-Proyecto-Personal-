7977ae5cf18b0c8ba881c5122eaf84393b645de0
David <davidrodrilopez2002@gmail.com>
2022-07-21 01:49:49 -0400

    Agregando lo hecho en el curso de HTML Y CSS + el proyecto de Botas Nike

---
+++ CSS/herencia/aaa.js
@@ -0,0 +1,8 @@
+function presentacion(name, lastName, nickname){
+
+    let completeName = name + lastName;
+    
+    return console.log("Mi nombre es " + completeName + ", pero prefiero que me digas " + nickname + ".")
+    }
+
+presentacion(David, Rodriguez, dabi2002)
